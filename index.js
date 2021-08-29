718a1ad54e818003497942fe8dc56b85d2a9225e
David MH Ryu <myeonghan12@gmail.com>
2021-08-29 23:34:31 +0900

    add actionCreator

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 import {createStore} from './redux.js'
 
-function reducer(state, data) {
-    if (data.type === 'count') {
-        return {...state, counter: data.payload.counter};
+// action (type)
+const COUNTER = 'count'
+
+function reducer(state, action) {
+    if (action.type === COUNTER) {
+        return {...state, counter: action.payload.counter};
     }
     return state;
 }
 
+function listener() {
+    console.log(store.getState())
+}
+
+function actionCreator(type, payload) {
+    return {
+        type,
+        payload
+    }
+}
+
 const store = createStore(reducer);
 
-
+store.subscribe(listener);
 
 // (뭘 수정할거야, 이렇게 수정해줘)
-store.dispatch({
-    type: 'count',
-    payload: {
-        counter: 1
-    },
-});
+store.dispatch(actionCreator(COUNTER, {  counter: 1}));
+store.dispatch(actionCreator(COUNTER, {  counter: 2}));
 
-console.log(store.getState())
--- redux.js
+++ redux.js
@@ -4,11 +4,17 @@
 // 수정 제공 불가
 // 리덕스는 수정 해야할 시점에 수정 함수를 호출만 해줘
 
+// 두 컴포넌트가 공통의 관심사를 갖고, 한 컴포넌트가 상태를 바꿀 때, 다른 한 컴포넌트는 그것을 어떻게 알 수 있는가? (예, addEventListener('click', function() )
+// => pop(s) up 패턴.
 export function createStore(reducer)  {
     let state;
+    const handler = [];
 
-    function dispatch(data){
-        state = reducer(state, data);
+    function dispatch(action){
+        state = reducer(state, action);
+        handler.forEach((listener) => {
+            listener();
+        })
 
     }
 
@@ -16,10 +22,16 @@ export function createStore(reducer)  {
         return state;
     }
 
+    function subscribe(listener) {
+        handler.push(listener);
+
+    }
+
     return {
         dispatch,
         // 직접 state 리턴하면 바깥에서 직접 수정 가능, doUpdate()통해서가 아닌.
         getState,
+        subscribe
     }
 
 }
